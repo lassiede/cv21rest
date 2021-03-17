f3d6962f3c08ea4714657f817ceb75ab7e51d82b
Eden LASSIRE <lassiede@inf-35-15.dptinfo.univ-rouen.fr>
2021-03-17 11:09:52 +0100

    commit cv 21 1

---
+++ src/main/java/fr/univrouen/cv21rest/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package fr.univrouen.cv21rest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "Hello cv21 !";
+	}
+
+}
